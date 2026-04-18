c745fe792693c12b329ea5e4bc2f02574695c650
Emi <emilia.green63@gmail.com>
2026-04-18 19:59:29 +0100

    glarb is alive

--- basic.py
+++ basic.py
@@ -2,6 +2,21 @@ import discord
 from discord.ext import commands
 import logging
 from dotenv import load_dotenv
+import random
+
+dinner_outcomes = [
+    "You eat dinner with Jay-Z. His business acumen is as sharp as his fashion sense, and you find yourself learning a lot about the music industry.",
+    "You try to get dinner with Jay-Z, but he runs late due to an appointment at the helicopter dealership.",
+    "You forgo the dinner and take the $50,000, using it to buy a solid gold CD copy of his 2008 album 'The Blueprint 3'.",
+    "Jay-Z flies into a rage because the gazpacho soup is too cold. You are unable to glean any useful information from the dinner.",
+    "Jay-Z explains to you how to whip coke over a stove using a Pyrex jug.",
+    "Jay-Z explains that the famous clip of him nodding to a Timbaland beat was actually staged with a series of lifelike puppets.",
+    "Jay-Z reveals that he is actually a time-traveling frog who has been manipulating the music industry for centuries, and that his real name is Glarb.",
+    "Jay-Z narrowly defeats you in a rock-paper-scissors best of 7.",
+    "You accidentally call Jay-Z 'Jay-G' and he is so offended that he cancels the dinner and leaves, leaving you with nothing but a $50,000 bill for the meal.",
+    "You play the Jay-Z 'Monster' verse parody off your phone, and he does not find it funny. He leaves the dinner early, and you are left with a $50,000 bill for the meal.",
+    "You play the Jay-Z 'Monster' verse parody off your phone, and he finds it hilarious. He laughs so hard that he accidentally chokes on his food and dies, leaving you with a $50,000 bill for the meal and a lifetime of guilt."
+
 
 class Basic(commands.Cog):
     def __init__(self, bot):
@@ -9,12 +24,22 @@ class Basic(commands.Cog):
 
     @commands.command(name="dinner")
     async def dinner(self, ctx):
-        await ctx.send("You eat dinner with Jay-Z Again.")
+        await ctx.send(dinner_outcomes[random.randint(0, len(dinner_outcomes) - 1)])
 
-    @commands.command(name="ping")
-    async def pingcmd(self, ctx):
-        """the best command in existence"""
-        await ctx.send(ctx.author.mention)
+    @commands.command(name="commands")
+    async def helpcmd(self, ctx):
+        await ctx.send(
+            "I am Glarb, the slipperiest sorcerer in the realm. I can play music and do some MtG related stuff. \n\n"
+            "**Commands:**\n"
+            "!play <video> - play a video\n"
+            "!skip - skip the current video\n"
+            "!stop - stop the music and disconnect\n"
+            "!loop <video> - loop a video\n"
+            "!playlist <name> - play a predefined playlist (battle or exploring)\n"
+            "!shuffle - shuffle the current playlist\n"
+            "!queue - show the current queue\n"
+            "!dinner - have dinner with Jay-Z\n"
+        )
 
     # doing something when the cog gets loaded
     async def cog_load(self):
@@ -25,8 +50,6 @@ class Basic(commands.Cog):
         print(f"{self.__class__.__name__} unloaded!")
 
 
-
-
 async def setup(bot):
-        # finally, adding the cog to the bot
+    # finally, adding the cog to the bot
     await bot.add_cog(Basic(bot=bot))
--- youtube.py
+++ youtube.py
@@ -2,6 +2,7 @@ import asyncio
 import discord
 from discord.ext import commands
 import yt_dlp
+import random
 
 
 ytdlp_options = {
@@ -30,15 +31,29 @@ class YTDLP(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.queue = []
+        self.looping = False
+
+    def voice_check(self, ctx):
+        if not ctx.author.voice:
+            return (
+                False,
+                "Foolish tadpole, you need to be in a voice channel to play music.",
+            )
+        return True, None
+
+    async def initialise_voice_client(self, ctx):
+        if ctx.guild.id not in voice_clients:
+            voice_client = await ctx.author.voice.channel.connect()
+            voice_clients[ctx.guild.id] = voice_client
 
     @commands.command()
-    async def play(self, ctx, playlist_name: str):
+    async def playlist(self, ctx, playlist_name: str):
 
-        if not ctx.author.voice:
-            await ctx.send(
-                "Foolish tadpole, you need to be in a voice channel to play music."
-            )
+        is_valid, error_message = self.voice_check(ctx)
+        if not is_valid:
+            await ctx.send(error_message)
             return
+
         if playlist_name not in playlists:
             await ctx.send(
                 "Invalid playlist name, foolish pond scum. Glarb is currently configured for two playlists: 'battle' and 'exploring'. Attach one of them at the end of !play."
@@ -48,9 +63,7 @@ class YTDLP(commands.Cog):
         url = playlists[playlist_name]
         loop = asyncio.get_event_loop()
 
-        if ctx.guild.id not in voice_clients:
-            voice_client = await ctx.author.voice.channel.connect()
-            voice_clients[ctx.guild.id] = voice_client
+        await self.initialise_voice_client(ctx)
 
         try:
 
@@ -81,12 +94,11 @@ class YTDLP(commands.Cog):
         try:
             if audio_url is None:
                 await ctx.send(
-                    "Glarb's augurs could not find a valid audio stream for this video. Moving on to the next one."
+                    "My augurs could not find a valid audio stream for this video. Moving on to the next one."
                 )
                 await self.play_next(ctx)
                 return
-            # voice_client = await ctx.author.voice.channel.connect()
-            # voice_clients[ctx.guild.id] = voice_client
+
             player = discord.FFmpegPCMAudio(audio_url, **ffmpeg_options)
             source = discord.PCMVolumeTransformer(player, volume=0.5)
 
@@ -102,43 +114,122 @@ class YTDLP(commands.Cog):
                 f"Despite the toil of my finest augurs, an error occurred while trying to play the audio: {str(e)}"
             )
 
+    async def play_next(self, ctx):
+
+        if not self.queue:
+            await ctx.send("The playlist has come to an end, my supplicant.")
+
+        entry = self.queue.pop(0)
+        if self.looping:
+            self.queue.append(entry)
+
+        try:
+            loop = asyncio.get_event_loop()
+            data = await loop.run_in_executor(
+                None, lambda: ytdl.extract_info(entry["url"], download=False)
+            )
+            if data is None:
+                await self.play_next(ctx)
+                return
+
+            audio_url = data["url"]
+        except Exception as e:
+            await ctx.send(f"Glarb could not divine the next track: {str(e)}")
+            await self.play_next(ctx)
+            return
+
+        player = discord.FFmpegPCMAudio(audio_url, **ffmpeg_options)
+        source = discord.PCMVolumeTransformer(player, volume=0.5)
+
+        if not self.looping:
+            await ctx.send(f"Now playing: {data.get('title', 'Unknown')}")
+        else:
+            await ctx.send(
+                f"My choir of toads are looping the following track: {data.get('title', 'Unknown')}"
+            )
+
+        loop = asyncio.get_event_loop()
+        voice_clients[ctx.guild.id].play(
+            source,
+            after=lambda e: (print(f"Player error: {e}") if e else None)
+            or asyncio.run_coroutine_threadsafe(self.play_next(ctx), loop),
+        )
+
+    @commands.command(name="skip")
+    async def skip(self, ctx):
+        if ctx.guild.id in voice_clients and voice_clients[ctx.guild.id].is_playing():
+            await ctx.send("Skipping to the next track.")
+            voice_clients[ctx.guild.id].stop()
+        else:
+            await ctx.send("There is no track currently playing, insect.")
+
+    @commands.command(name="loop")
+    async def loop(self, ctx, video: str):
+        is_valid, error_message = self.voice_check(ctx)
+        if not is_valid:
+            await ctx.send(error_message)
+            return
+        self.queue = [{"url": video}]
+        self.looping = True
+        await self.initialise_voice_client(ctx)
+        await self.play_next(ctx)
+
+    @commands.command(name="play")
+    async def play(self, ctx, video: str):
+        is_valid, error_message = self.voice_check(ctx)
+        if not is_valid:
+            await ctx.send(error_message)
+            return
+        if ctx.guild.id in voice_clients and voice_clients[ctx.guild.id].is_playing():
+            self.queue.append({"url": video})
+            await ctx.send("The track has been added to the queue, my tadpole.")
+            return
+        self.queue = [{"url": video}]
+        self.looping = False
+        await self.initialise_voice_client(ctx)
+        await self.play_next(ctx)
+
     @commands.command(name="stop")
     async def stop(self, ctx):
         if ctx.guild.id in voice_clients:
             self.queue = []
+            self.looping = False
             voice_clients[ctx.guild.id].stop()
             await voice_clients[ctx.guild.id].disconnect()
             del voice_clients[ctx.guild.id]
             await ctx.send("Going back to my pond.")
 
-    async def play_next(self, ctx):
-        if self.queue:
-            entry = self.queue.pop(0)
+    @commands.command(name="shuffle")
+    async def shuffle(self, ctx):
+        if not self.queue:
+            await ctx.send("The playlist is currently empty, my tadpole.")
+            return
 
+        random.shuffle(self.queue)
+        await ctx.send("The order of the playlist has been shuffled, my tadpole.")
+
+    @commands.command(name="queue")
+    async def show_queue(self, ctx):
+        if not self.queue:
+            await ctx.send("The playlist is currently empty, my tadpole.")
+            return
+
+        queue_titles = []
+        loop = asyncio.get_event_loop()
+        for entry in self.queue:
             try:
-                loop = asyncio.get_event_loop()
                 data = await loop.run_in_executor(
                     None, lambda: ytdl.extract_info(entry["url"], download=False)
                 )
-                if data is None:
-                    await self.play_next(ctx)
-                    return
-
-                audio_url = data["url"]
+                title = data.get("title", "Unknown")
+                queue_titles.append(title)
             except Exception as e:
-                await ctx.send(f"Glarb could not divine the next track: {str(e)}")
-                await self.play_next(ctx)
-                return
+                queue_titles.append("Unknown")
 
-            player = discord.FFmpegPCMAudio(audio_url, **ffmpeg_options)
-            source = discord.PCMVolumeTransformer(player, volume=0.5)
-            await ctx.send(f"Now playing: {data.get('title', 'Unknown')}")
-            loop = asyncio.get_event_loop()
-            voice_clients[ctx.guild.id].play(
-                source,
-                after=lambda e: (print(f"Player error: {e}") if e else None)
-                or asyncio.run_coroutine_threadsafe(self.play_next(ctx), loop),
-            )
+        queue_message = "Current playlist:\n" + "\n".join(
+            f"{idx + 1}. {title}" for idx, title in enumerate(queue_titles)
+        )
+        await ctx.send(queue_message)
 
 
 async def setup(bot):
