69e20d0b20fb75ca3e28f12544d06bc50ecf8ab7
halicasoft <halicasoft@gmail.com>
2026-04-22 23:21:07 +0100

    status maybe

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Glarb(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
-        super().__init__(command_prefix="!", intents=intents)
+        super().__init__(command_prefix="!", intents=intents, activity=discord.Game("on his lilypad"))
         
 
     async def setup_hook(self):
